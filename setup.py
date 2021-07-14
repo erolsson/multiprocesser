cda7f22f108443a1b236f1b8de3984f9df17972e
erolsson@kth.se <erolsson@kth.se>
2021-07-14 19:50:20 +0200

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='multiprocesser',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='erolsson',
+    author_email='erolsson@kth.se',
+    description=''
+)
